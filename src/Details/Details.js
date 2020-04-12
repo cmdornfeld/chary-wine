4de28a0caa265e6fd70bc6c28763432fafb687bd
Chad Dornfeld <cmdornfeld@gmail.com>
2020-04-12 11:18:19 -0500

    added skeleton for Details component

---
+++ src/Details/Details.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
+
+class Details extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Details;
