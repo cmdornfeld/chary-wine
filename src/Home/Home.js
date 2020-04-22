9cae80280b78ece95c0f9d444421a6f763924ae8
Chad Dornfeld <cmdornfeld@gmail.com>
2020-04-22 13:57:12 -0500

    added viewWine function for Details view of selected wine

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import Header from '../Header/Header';
 // import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -12,30 +12,37 @@ class Home extends Component {
         this.props.dispatch({type: 'GET_WINES'});
       }
 
+      viewWine = (id) => {
+        this.props.history.push(`/wines/details/${id}`);
+    }
+
     render() {
         return (
             <div>
-                {this.props.wines.map(wine => {
-                    return (
-                        <Card className="wine-card" key={wine.id}>
-                            <CardContent>
-                            <Typography>
-                                <h2>{wine.brand}</h2>
-                            </Typography>
-                            <br/>
-                            {/* // on the click of the "movie.poster"/img, "link" user to the details page
-                                // trigger goToDetails function with given parameters (event and the id of the movie clicked) */}
-                            {/* <Link to='/details'> */}
-                            <img onClick={(event) => this.goToDetails(event, wine.id)} src={wine.image}
-                                alt={wine.brand} /> 
-                            {/* </Link> */}
-                            <Typography component="p">
-                                {wine.description}
-                            </Typography>
-                            </CardContent>
-                        </Card>
-                    )
-                })}
+                <Header />
+                <div className="wine-card-container">
+                    {this.props.wines.map(wine => {
+                        return (
+                            <Card className="wine-card" key={wine.id}
+                                onClick={() => this.viewWine(wine.id)}>
+                                <CardContent>
+                                {/* // on the click of the "movie.poster"/img, "link" user to the details page
+                                    // trigger goToDetails function with given parameters (event and the id of the movie clicked) */}
+                                {/* <Link to='/details'> */}
+                                <img src={wine.image} alt={wine.brand} width="200" /> 
+                                {/* </Link> */}
+                                <br/>
+                                <h2>{wine.brand} {wine.name}</h2>
+                                <br/>
+                                <Typography component="p">
+                                    {wine.description}
+                                </Typography>
+                                <h3>Rating: {Number(wine.totalRating).toFixed(2)}</h3>
+                                </CardContent>
+                            </Card>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
