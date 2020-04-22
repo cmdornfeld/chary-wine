3bfdb4eedc04ac6322ed292f7eb7fd42b1967d6b
Chad Dornfeld <cmdornfeld@gmail.com>
2020-04-22 13:57:46 -0500

    inlcuded image and details reducer in Details.js

--- src/Details/Details.js
+++ src/Details/Details.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import Header from '../Header/Header';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 class Details extends Component {
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_WINE_DETAILS', payload: this.props.match.params.id })
+    }
+
     render() {
         return (
             <div>
-                
+                <Header />
+                <div className="wine-card-container">
+                    <Card>
+                        <CardContent>
+                            <img src={this.props.details.image} alt={this.props.details.brand} width="400" /> 
+                        </CardContent>
+                    </Card>
+                </div>          
             </div>
         )
     }
 }
 
-export default Details;
+const putReduxStateOnProps = (reduxState)=>({
+    details: reduxState.detailsReducer
+  });
+
+export default connect(putReduxStateOnProps)(Details);
